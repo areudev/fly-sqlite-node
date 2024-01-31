ad339b5309eee688ce49e09c8c813cad21fe5514
Chris Lime <chrkalivas@gmail.com>
2024-01-31 11:05:52 +0200

    Refactor code to use single quotes and remove unnecessary semicolons

--- app/index.ts
+++ app/index.ts
@@ -1,62 +1,62 @@
-import http from "http";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import http from 'http'
+import {PrismaClient} from '@prisma/client'
+const prisma = new PrismaClient()
 
 async function getCurrentCount() {
-  let currentCount = await prisma.count.findFirst();
+  let currentCount = await prisma.count.findFirst()
   if (!currentCount) {
     currentCount = await prisma.count.create({
-      data: { count: 0 },
-    });
+      data: {count: 0},
+    })
   }
-  return currentCount;
+  return currentCount
 }
 
 async function parseFormBody(req: http.IncomingMessage) {
   const body = await new Promise<string>((resolve) => {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      resolve(body);
-    });
-  });
-  const params = new URLSearchParams(body);
-  return params;
+    let body = ''
+    req.on('data', (chunk) => {
+      body += chunk
+    })
+    req.on('end', () => {
+      resolve(body)
+    })
+  })
+  const params = new URLSearchParams(body)
+  return params
 }
 
 const server = http
   .createServer(async (req, res) => {
-    console.log(`${req.method} ${req.url}`);
+    console.log(`${req.method} ${req.url}`)
 
     switch (`${req.method} ${req.url}`) {
-      case "POST /": {
-        const params = await parseFormBody(req);
-        const intent = params.get("intent");
-        const currentCount = await getCurrentCount();
-        if (intent !== "increment" && intent !== "decrement") {
-          return res.end("Invalid intent");
+      case 'POST /': {
+        const params = await parseFormBody(req)
+        const intent = params.get('intent')
+        const currentCount = await getCurrentCount()
+        if (intent !== 'increment' && intent !== 'decrement') {
+          return res.end('Invalid intent')
         }
         await prisma.count.update({
-          where: { id: currentCount.id },
-          data: { count: { [intent]: 1 } },
-        });
-        res.writeHead(302, { Location: "/" });
-        res.end();
-        break;
+          where: {id: currentCount.id},
+          data: {count: {[intent]: 1}},
+        })
+        res.writeHead(302, {Location: '/'})
+        res.end()
+        break
       }
-      case "GET /": {
-        let currentCount = await getCurrentCount();
-        res.setHeader("Content-Type", "text/html");
-        res.writeHead(200);
+      case 'GET /': {
+        let currentCount = await getCurrentCount()
+        res.setHeader('Content-Type', 'text/html')
+        res.writeHead(200)
         res.end(/* html */ `
 <html>
   <head>
     <title>Demo App</title>
   </head>
   <body>
-    <h1>Demo App</h1>
+    <h1>Best fuckn counter</h1>
     <form method="POST">
       <button type="submit" name="intent" value="decrement">-</button>
       <span>${currentCount.count}</span>
@@ -64,24 +64,22 @@ const server = http
     </form>
   </body>
 </html>
-        `);
-        break;
+        `)
+        break
       }
       default: {
-        res.writeHead(404);
-        return res.end("Not found");
+        res.writeHead(404)
+        return res.end('Not found')
       }
     }
   })
   .listen(process.env.PORT, () => {
-    const address = server.address();
+    const address = server.address()
     if (!address) {
-      console.log("Server listening");
-      return;
+      console.log('Server listening')
+      return
     }
     const url =
-      typeof address === "string"
-        ? address
-        : `http://localhost:${address.port}`;
-    console.log(`Server listening at ${url}`);
-  });
+      typeof address === 'string' ? address : `http://localhost:${address.port}`
+    console.log(`Server listening at ${url}`)
+  })
